06146c5b7d797c821aab8a027fcd09a9d22d3290
vishal-yadav-git <168914300+vishal-yadav-git@users.noreply.github.com>
2024-12-03 06:14:34 -0800

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,14 +1,34 @@
+"""
+URL configuration for hello project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path
-from home import views
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header = "CNG"
+admin.site.site_title = "CNG 2"
+admin.site.index_title = "Welcome to CNG Portal"
 
 urlpatterns = [
-    path("", views.index,name='home'),
-    path("Product/", views.Product,name='Product'),
-    path("Pricing/", views.Pricing,name='Pricing'),
-    path("About/", views.About,name='About'),
-    path("Contect/", views.Contect,name='Contect'),
-    path("Page/", views.Page,name='Page'),
-    path("login/", views.login_view,name='login'),
-    path("signup/", views.signup,name='signup'),
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('signup/', include('home.urls')),
+    path('login/', include('home.urls')),
 ]
+# add media file handling in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
